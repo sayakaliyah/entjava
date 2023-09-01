4004db6f2fdfcbb5664dd51797fe5e822354cc70
sayakaliyah <116573858+sayakaliyah@users.noreply.github.com>
2023-09-02 01:45:46 +0800

    Add files via upload

---
+++ NETCore/SampleWebApiAspNetCore/Dtos/FoodUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SampleWebApiAspNetCore.Dtos
+{
+    public class FoodUpdateDto
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public int Price { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ NETCore/SampleWebApiAspNetCore/Services/SeedDataService.cs
@@ -0,0 +1,18 @@
+﻿using SampleWebApiAspNetCore.Entities;
+using SampleWebApiAspNetCore.Repositories;
+
+namespace SampleWebApiAspNetCore.Services
+{
+    public class SeedDataService : ISeedDataService
+    {
+        public void Initialize(FoodDbContext context)
+        {
+            context.FoodItems.Add(new FoodEntity() { Price = 100, Type = "Dessert", Name = "Cookies", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { Price = 200, Type = "Snacks", Name = "Fries", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { Price = 300, Type = "Main", Name = "Carbonara", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { Price = 400, Type = "Main", Name = "Steak", Created = DateTime.Now });
+
+            context.SaveChanges();
+        }
+    }
+}
